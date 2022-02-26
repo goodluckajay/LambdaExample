74b1e1194fa1e8946ae1bdd27d9741907dacfdcf
Ajay Dhiman <goodluckajay@gmail.com>
2022-02-26 14:31:09 +0530

    Update ResourceDB.java

--- src/com/self/flatmap/ResourceDB.java
+++ src/com/self/flatmap/ResourceDB.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// Sample DB for testing
 public class ResourceDB {
 
     public static List<Resource> getAllResource(){
